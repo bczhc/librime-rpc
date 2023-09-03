4e7d6a097e522d6bac9be5307b23e430503f5708
Zhai Can <bczhc0@126.com>
2023-09-03 18:45:36 +0800

    handle Ctrl-C

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use std::net::SocketAddr;
+use std::sync::mpsc::channel;
+
 use jsonrpsee::server::Server;
 use jsonrpsee::RpcModule;
+
 use librime_rpc::methods;
-use std::net::SocketAddr;
-use std::sync::{Condvar, Mutex};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let port = 8001;
-    let server = Server::builder()
-        .build(SocketAddr::from(([0, 0, 0, 0], port)))
-        .await?;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let server = Server::builder().build(addr).await?;
     let mut module = RpcModule::new(());
 
     macro_rules! register_method {
@@ -19,11 +20,18 @@ async fn main() -> anyhow::Result<()> {
     }
     register_method!("version", methods::version);
 
-    let _handle = server.start(module);
+    let handle = server.start(module);
 
-    // the RPC server will be shutdown on `handle` dropped; hangs the program
-    let mutex = Mutex::new(());
-    let _guard = Condvar::new().wait(mutex.lock().unwrap()).unwrap();
+    println!("Server started on {}", addr);
+
+    let (tx, rx) = channel();
+    ctrlc::set_handler(move || {
+        tx.send(()).unwrap();
+    })
+    .unwrap();
+    rx.recv().unwrap();
+    println!("Ctrl-C received; exiting...");
+    handle.stop()?;
 
     Ok(())
 }
