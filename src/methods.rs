0ef0d01628f76bbff085ebf8b6b1803a8badbbac
Zhai Can <bczhc0@126.com>
2023-09-03 18:38:00 +0800

    rpc method: version

--- src/methods.rs
+++ src/methods.rs
@@ -1,6 +1,12 @@
-use jsonrpsee::IntoResponse;
 use jsonrpsee::types::Params;
+use jsonrpsee::IntoResponse;
+use std::ffi::CStr;
 
 pub fn version(_p: Params) -> impl IntoResponse {
-    "TODO"
+    unsafe {
+        let api = librime_sys::rime_get_api();
+        CStr::from_ptr((*api).get_version.unwrap()())
+            .to_str()
+            .expect("Invalid UTF-8")
+    }
 }
